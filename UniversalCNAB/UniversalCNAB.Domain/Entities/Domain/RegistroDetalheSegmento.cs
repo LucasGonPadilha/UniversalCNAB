4edcbca74600d5a1beaadc596ddf78dccc9cd6a2
Daniel Nicodemos <dani_nicodemos@live.com>
2022-07-12 00:03:40 -0300

    subida inicial arquitetura

---
+++ UniversalCNAB/UniversalCNAB.Domain/Entities/Domain/RegistroDetalheSegmento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalCNAB.Domain.Entities.Domain
+{
+    public class RegistroDetalheSegmento
+    {
+        public int Tipo { get { return 3; } }
+    }
+}
---
+++ UniversalCNAB/UniversalCNAB.Domain/Entities/Domain/RegistroHeaderLote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalCNAB.Domain.Entities.Domain
+{
+    public class RegistroHeaderLote
+    {
+        public int Tipo { get { return 1; } }
+    }
+}
